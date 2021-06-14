259915784fe74f9389af903126b9755221ac59c7
Roman-wdesign <gabbasov.tomsk@gmail.com>
2021-06-14 08:11:18 +0700

    2

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -7,30 +7,19 @@
         <p> name: {{ person.name }}</p>
         <button v-on:click="counter += 1">+1</button>
       </div>
+      <div class="img"
+           style=" width: 20px;height: 20px;"
+      ><img :src="images">
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
-// let urls = [
-//       'https://swapi.dev/api/people/1/',
-//       'https://swapi.dev/api/people/2/',
-//       'https://swapi.dev/api/people/3/',
-//       'https://swapi.dev/api/people/4/',
-//       'https://swapi.dev/api/people/5/',
-//       'https://swapi.dev/api/people/6/',
-//       'https://swapi.dev/api/people/7/',
-//       'https://swapi.dev/api/people/8/',
-//       'https://swapi.dev/api/people/9/',
-//       'https://swapi.dev/api/people/10/'
-//     ];
-// let requests = urls.map(url => axios(url));
-//
-//     Promise.all(requests)
-//         .then(responses => responses.forEach(
-//             response => alert(`${response.url}: ${response.status}`)
-//         ));
+
+
+
 
 export default {
   name: "Main",
@@ -38,6 +27,9 @@ export default {
     counter: 0,
     people: [],
     id: Number,
+    images: [],
+    image:String
+
 
   }),
   props: {},
@@ -62,10 +54,30 @@ export default {
             }
             console.log(error.config);
           })
+    },
+    loadImages() {
+      axios.get('https://starwars-visualguide.com/assets/img/characters/1.jpg')
+          .then(response => {
+            console.log(this.images = response)
+          })
+          .finally(() => console.log('%cData people loading is complete', 'background: #183CA8FF; color: #FFFFFFFF'))
+          .catch(function (error) {
+            if (error.response) {
+              console.log(error.response.data);
+              console.log(error.response.status);
+              console.log(error.response.headers);
+            } else if (error.request) {
+              console.log(error.request);
+            } else {
+              console.log('Error', error.message);
+            }
+            console.log(error.config);
+          })
     }
   },
   mounted() {
     this.loadPeople();
+    this.loadImages();
   },
   watch: {},
   computed: {
@@ -96,6 +108,7 @@ export default {
 }
 
 .card {
+  //color: #b7d02c;
   margin: 10px;
   padding: 10px;
   height: 70px;
